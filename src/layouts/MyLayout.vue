78259479132978c30a55ac3b827605589fca6201
amnuay <ch.amnuay@gmail.com>
2022-07-27 06:10:15 +0700

    Create MyLayout.vue

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,6 @@
+//Replace from
+     var keycloakAuth = this.$store.getters.SECURITY_AUTH
+     keycloakAuth.logout()
+//Replace to
+           const { userManager } = this.$store.getters.SECURITY_AUTH
+           userManager.signoutRedirect()
