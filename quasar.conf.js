7cf83376787ab1524b0dbcabf584bd28701dd678
amnuay <ch.amnuay@gmail.com>
2022-07-27 06:03:16 +0700

    Create quasar.conf.js

---
+++ quasar.conf.js
@@ -0,0 +1,5 @@
+env: {
+       api: xxx,
+       ONELOGIN_SUBDOMAIN: JSON.stringify(process.env.ONELOGIN_SUBDOMAIN),
+       ONELOGIN_CLIENT_ID: JSON.stringify(process.env.ONELOGIN_CLIENT_ID)
+     }
